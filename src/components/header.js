0e41083feda7f68b604f3f86f336538515845282
bhenbe <contact@bhen.be>
2019-11-25 09:34:58 +0100

    update accessibility for navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,29 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import Toggle from "../components/toggle"
 import siteLogo from "../assets/img/logo-supersimple-vertical.svg"
 import "../assets/scss/styles.scss"
 
 function Header({ siteTitle }) {
   const [navbarState, setNavbarState] = useState(false);
-
+  const firstElement = useRef(null);
+  const secondElement = useRef(null);
+  
   function handleToggleClick(ariaPressedState) {
     // Au changement du togge,
     // on recupère son état,
     // et on l'assigne à l'état de la navbar
     setNavbarState(ariaPressedState)
+
+    if (ariaPressedState === true) {
+      firstElement.current.tabIndex = 0
+      secondElement.current.tabIndex = 0
+      firstElement.current.focus()
+    } else {
+      firstElement.current.tabIndex = -1
+      secondElement.current.tabIndex = -1
+    }
   }
 
   return (
@@ -20,11 +31,11 @@ function Header({ siteTitle }) {
       <div className="container">
         <nav id="main-menu" className={`navbar ${navbarState ? 'navbar--active' : ''}`}>
           <ul>
-            <li id="brand"><Link to="/"><img src={siteLogo} alt={siteTitle} /></Link></li>
-            <li><Link to="/services">Nos services</Link></li>
-            <li><Link to="/contact">Nous contacter</Link></li>
+            <li id="brand" className="base-li"><Link to="/"><img src={siteLogo} alt={siteTitle} /></Link></li>
+            <li id="toggle" className="base-li"><Toggle onToggleUpdate={handleToggleClick} /></li>
+            <li><Link to="/services" tabIndex="-1" ref={firstElement}>Nos services</Link></li>
+            <li><Link to="/contact" tabIndex="-1" ref={secondElement}>Nous contacter</Link></li>
           </ul>
-          <Toggle onToggleUpdate={handleToggleClick} />
         </nav>
       </div>
     </header>
